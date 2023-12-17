04ed4ee06b3e933e89943990e2c9c06b2acc36cb
Igor Wlodarczyk <wlodar.igor@gmail.com>
2023-12-17 16:44:44 +0100

    0.2.0

--- main.cpp
+++ main.cpp
@@ -18,9 +18,15 @@ using namespace std;
 struct Graph {
     int number;
     int size;
-    vector<vector<int>> adjacencyMatrix;
+    vector<vector<int> > adjacencyMatrix;
 };
 
+void display_permutation(vector<int> permutation){
+    for(int i = 0; i < permutation.size(); i++){
+        cout << permutation[i] << "";
+    }
+    cout << endl;
+}
 Graph create_graph_from_file(string filename){
     ifstream input_file(filename);
     Graph graph;
@@ -48,16 +54,16 @@ void display_graph(Graph graph){
     }
 }
 
-void two_opt_swap(vector<int>& temp_permutation){
-    srand((unsigned)time(NULL));
+vector<int> two_opt_swap(vector<int> temp_permutation){
+    
     int first_city = -1;
     int second_city = -1;
-    while(first_city != second_city){
+    while(first_city == second_city){
         first_city = rand() % temp_permutation.size();
 	    second_city = rand() % temp_permutation.size();
     }
     swap(temp_permutation[first_city], temp_permutation[second_city]);
-
+    return temp_permutation;
 }
 
 int calculate_path(vector<int> permutation, Graph graph){
@@ -99,14 +105,14 @@ int simulated_annealing(Graph graph, int cooling_type, int type, double factor, 
     int temp_path_length;
     int delta;
     uniform_real_distribution<double> dis(0.0, 1.0);
+    
     while(temperature > absolute_temperature){
         int epoch = 0;
         while(epoch < max_epoch){
             if(type == 1){
                 temp_permutation = permutation;
-                two_opt_swap(temp_permutation);
+                temp_permutation = two_opt_swap(temp_permutation);
             }
-
             temp_path_length = calculate_path(temp_permutation, graph);
             delta = temp_path_length - shortest_path_length;
             if(delta < 0){
@@ -122,6 +128,7 @@ int simulated_annealing(Graph graph, int cooling_type, int type, double factor, 
             }
             epoch += 1;
         }
+    
         if(cooling_type == 1){
             temperature = geometrical_cooling(temperature, factor);
         }else if(cooling_type == 2){
@@ -131,9 +138,10 @@ int simulated_annealing(Graph graph, int cooling_type, int type, double factor, 
     }
     return shortest_path_length;
 }
+    
 
-vector<pair<string, int>> get_tested_graphs(string filename){
-    vector<pair<string, int>> tested_graphs;
+vector<pair<string, int> > get_tested_graphs(string filename){
+    vector<pair<string, int> > tested_graphs;
     ifstream file(filename);
     string line;
     bool inside_file_content = false;
@@ -162,23 +170,218 @@ vector<pair<string, int>> get_tested_graphs(string filename){
      return tested_graphs;
 }
 
+// Base parameters
+int cooling_type;
+double factor;
+double step;
+int max_epoch;
+double starting_temperature;
+void baseline(){
+    cooling_type = 1;
+    factor = 0.97;
+    step = 0.01;
+    max_epoch = 150;
+}
+
+unsigned long long silnia(int n) {
+    if (n == 0 || n == 1) {
+        return 1;
+    } else {
+        return n * silnia(n - 1);
+    }
+}
+
+string generate_file_name(string parameter, double value){
+    ostringstream double_stream;
+    double_stream << value;
+    string double_string = double_stream.str();
+    for(char &c : double_string){
+        if(c == '.'){
+            c = '_';
+        }
+    }
+    return parameter += double_string + ".txt";
+}
+
 void tests(){
-    vector<pair<string, int>> result = get_tested_graphs("config.ini");
+    struct timeval start, stop;
+
+    vector<pair<string, int> > result = get_tested_graphs("config.ini");
+    int number_of_tests = 5;
+
+    // Factor
+
+    cout << "Testowanie wplywu factor na wyniki" << endl;
+    baseline();
+    for(double factor = 0.92; factor < 1; factor += 0.01){
+        cout << "Wartosc factor " << factor << endl;
+        string filename = generate_file_name("factor", factor);
+        ofstream file("results/factor/" + filename);
+        cout << filename << endl;
+        for (const auto &pair : result) {
+        cout << "Filename: " << pair.first << ", Number: " << pair.second<< endl;
+        double avg = 0;
+        double avg_time = 0;
+        Graph graph = create_graph_from_file("graphs/" + pair.first);
+        for(int i = 0; i < number_of_tests; i ++){
+            starting_temperature = 30 * graph.size;
+            gettimeofday(&start, NULL);
+            int solution = simulated_annealing(graph, cooling_type, 1, factor, step, max_epoch, starting_temperature);
+            gettimeofday(&stop, NULL);
+            double time = (stop.tv_sec - start.tv_sec) + (stop.tv_usec - start.tv_usec) / 1e6;
+            avg_time += time;
+            avg += solution;
+            }
+        avg = avg / number_of_tests;
+        avg_time = avg_time / number_of_tests;
+        cout << avg << endl;
+        cout << (avg / pair.second - 1) * 100 << endl;
+        cout << avg_time << " s" << endl;
+        file << graph.size << ";" << avg_time << ";" << (avg / pair.second - 1) * 100 << endl;
+        }
+        file.close();
+    }
+    
 
     
-    for (const auto &pair : result) {
+    // Max epoch
+    cout << "Testowanie wplywu max_epoch na wyniki" << endl;
+    baseline();
+    for(int max_epoch = 50; max_epoch < 160; max_epoch += 20){
+        cout << "Wartosc max_epoch " << max_epoch << endl;
+        string filename = generate_file_name("max_epoch", max_epoch);
+        ofstream file("results/max_epoch/" + filename);
+        cout << filename << endl;
+        for (const auto &pair : result) {
         cout << "Filename: " << pair.first << ", Number: " << pair.second<< endl;
+        double avg = 0;
+        double avg_time = 0;
+        Graph graph = create_graph_from_file("graphs/" + pair.first);
+        for(int i = 0; i < number_of_tests; i ++){
+            starting_temperature = 30 * graph.size;
+            gettimeofday(&start, NULL);
+            int solution = simulated_annealing(graph, cooling_type, 1, factor, step, max_epoch, starting_temperature);
+            gettimeofday(&stop, NULL);
+            double time = (stop.tv_sec - start.tv_sec) + (stop.tv_usec - start.tv_usec) / 1e6;
+            avg_time += time;
+            avg += solution;
+            }
+        avg = avg / number_of_tests;
+        avg_time = avg_time / number_of_tests;
+        cout << avg << endl;
+        cout << (avg / pair.second - 1) * 100 << endl;
+        cout << avg_time << " s" << endl;
+        file << graph.size << ";" << avg_time << ";" << (avg / pair.second - 1) * 100 << endl;
+        }
+        file.close();
+    }
+    
+    
+    // Testowanie wplywu step na wyniki
+    cout << "Testowanie wplywu step na wyniki" << endl;
+    baseline();
+    cooling_type = 2;
+    for(double step = 0.9; step > 0.3; step -= 0.1){
+        cout << "Wartosc step " << step << endl;
+        string filename = generate_file_name("step", step);
+        ofstream file("results/step/" + filename);
+        cout << filename << endl;
+        for (const auto &pair : result) {
+        
+        cout << "Filename: " << pair.first << ", Number: " << pair.second<< endl;
+        double avg = 0;
+        double avg_time = 0;
+        Graph graph = create_graph_from_file("graphs/" + pair.first);
+        if(graph.size < 150){
+        for(int i = 0; i < number_of_tests; i ++){
+            starting_temperature = 30 * graph.size;
+            gettimeofday(&start, NULL);
+            int solution = simulated_annealing(graph, cooling_type, 1, factor, step, max_epoch, starting_temperature);
+            gettimeofday(&stop, NULL);
+            double time = (stop.tv_sec - start.tv_sec) + (stop.tv_usec - start.tv_usec) / 1e6;
+            avg_time += time;
+            avg += solution;
+            }
+        avg = avg / number_of_tests;
+        avg_time = avg_time / number_of_tests;
+        cout << avg << endl;
+        cout << (avg / pair.second - 1) * 100 << endl;
+        cout << avg_time << " s" << endl;
+        file << graph.size << ";" << avg_time << ";" << (avg / pair.second - 1) * 100 << endl;
+        }else cout << "Pass" << endl;
+        }
+        file.close();
+    }
+    
+
+    baseline();
+    string filename = generate_file_name("starting_temperature", 30);
+    ofstream file("results/starting_temperature/" + filename);
+    cout << filename << endl;
+    for (const auto &pair : result) {
+    cout << "Filename: " << pair.first << ", Number: " << pair.second<< endl;
+    double avg = 0;
+    double avg_time = 0;
+    Graph graph = create_graph_from_file("graphs/" + pair.first);
+    starting_temperature = 30 * graph.size;
+    cout << "Temperatura startowa " << starting_temperature << endl;
+    for(int i = 0; i < number_of_tests; i ++){
+        gettimeofday(&start, NULL);
+        int solution = simulated_annealing(graph, cooling_type, 1, factor, step, max_epoch, starting_temperature);
+        gettimeofday(&stop, NULL);
+        double time = (stop.tv_sec - start.tv_sec) + (stop.tv_usec - start.tv_usec) / 1e6;
+        avg_time += time;
+        avg += solution;
+        }
+    avg = avg / number_of_tests;
+    avg_time = avg_time / number_of_tests;
+    cout << avg << endl;
+    cout << (avg / pair.second - 1) * 100 << endl;
+    cout << avg_time << " s" << endl;
+    file << graph.size << ";" << avg_time << ";" << (avg / pair.second - 1) * 100 << endl;
+    }
+    file.close();
+
+
+    // Testowanie wplywu temperatury startowej na wyniki
+    if(true){
+    cout << "Testowanie wplywu temperatury startowej na wyniki" << endl;
+    baseline();
+    string filename = generate_file_name("starting_temperature", 900);
+    ofstream file("results/starting_temperature/" + filename);
+    cout << filename << endl;
+    for (const auto &pair : result) {
+    cout << "Filename: " << pair.first << ", Number: " << pair.second<< endl;
+    double avg = 0;
+    double avg_time = 0;
+    Graph graph = create_graph_from_file("graphs/" + pair.first);
+    starting_temperature = 30 * graph.size * graph.size;
+    cout << "Temperatura startowa " << starting_temperature << endl;
+    for(int i = 0; i < number_of_tests; i ++){
+        gettimeofday(&start, NULL);
+        int solution = simulated_annealing(graph, cooling_type, 1, factor, step, max_epoch, starting_temperature);
+        gettimeofday(&stop, NULL);
+        double time = (stop.tv_sec - start.tv_sec) + (stop.tv_usec - start.tv_usec) / 1e6;
+        avg_time += time;
+        avg += solution;
+        }
+    avg = avg / number_of_tests;
+    avg_time = avg_time / number_of_tests;
+    cout << avg << endl;
+    cout << (avg / pair.second - 1) * 100 << endl;
+    cout << avg_time << " s" << endl;
+    file << graph.size << ";" << avg_time << ";" << (avg / pair.second - 1) * 100 << endl;
+    }
+    file.close();
     }
 
+    
 
 }
 
 int main(){
-    Graph graph = create_graph_from_file("tsp.txt");
-
-    // int solution = simulated_annealing(graph, 2, 1, 1, 1, 1);
+    srand((unsigned)time(NULL));
     tests();
     return 0;
-
 
 }
